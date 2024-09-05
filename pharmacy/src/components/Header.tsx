f085ee75c41ec754c5b776c498c3836dd36dfc23
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-09-05 12:20:13 +0200

    05/09/2024

--- pharmacy/src/components/Header.tsx
+++ pharmacy/src/components/Header.tsx
@@ -1,25 +1,61 @@
 import { A } from "@solidjs/router";
 import "../styles/styles.css";
+import Searchbar from "./Searchbar";
 
 export default function Header() {
   const pages = ["Merken", "Ons Advies", "Partners", "Contact"];
+  const sections = ["Soorten", "Voeding", "Kinderen", "Dieren"];
 
   return (
-    <div class="bg-green-500 w-full p-7 flex justify-between">
-      <div class="bg-red-200 w-auto">l</div>
-      <div class="flex">
-        <div class="btn-group">
-          {pages.map((page) => (
-            <A href={`/${page}`} class="btn btn-default">
-              {page}
-            </A>
-          ))}
+    <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+      />
+
+      <div class="bg-green-500 w-full px-7 py-4 flex justify-between">
+        <div class="text-white w-auto p-2 mx-10 text-6xl font-bold align-middle cursor-default">
+          PHARMACY
         </div>
-        <div class="btn-group">
-          <button class="btn btn-login">Log In</button>
-          <button class="btn btn-register">Registreer</button>
+        <div class="flex">
+          <div class="btn-group">
+            {pages.map((page) => (
+              <A href={`/${page}`} class="btn btn-default text-lg">
+                {page}
+              </A>
+            ))}
+          </div>
+          <div class="btn-group">
+            <button class="btn btn-login">Log In</button>
+            <button class="btn btn-register">Registreer</button>
+          </div>
         </div>
       </div>
-    </div>
+      <div class="bg-green-100 w-full flex justify-center items-center space-x-20">
+        <div class="flex justify-center items-center">
+          <Searchbar />
+          <div class="btn-group">
+            {sections.map((section) => (
+              <>
+                <A href={`/${section}`} class="btn btn-secundary">
+                  {section}
+                </A>
+                <span class="text-gray-400">|</span>
+              </>
+            ))}
+          </div>
+        </div>
+        <div class="btn-group space-x-8">
+          <span class="material-symbols-outlined text-green-500">favorite</span>
+          <span class="material-symbols-outlined bg-green-500 text-white p-2 rounded">
+            shopping_cart
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ pharmacy/src/components/Searchbar.tsx
@@ -0,0 +1,30 @@
+const Searchbar = () => {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+      />
+
+      <div>
+        <form action="" class="w-full max-w-md">
+          <div class="relative flex items-center text-gray-400 focus-within:text-gray-500">
+            <span class="material-symbols-outlined absolute px-2 pointer-events-none">
+              search
+            </span>
+            <input
+              type="text"
+              name="search"
+              placeholder="Zoek..."
+              autocomplete="off"
+              aria-label="Zoek"
+              class="w-full placeholder-gray-400 ring-1 ring-green-600 px-2 py-1 pl-10 rounded-xl border-none focus:placeholder-gray-500 focus:ring-green-800 focus:ring-2"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Searchbar;
